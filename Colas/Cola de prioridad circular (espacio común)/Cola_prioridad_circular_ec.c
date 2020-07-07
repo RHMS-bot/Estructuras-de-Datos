ffbfe7aa9ae3e989d4e4d89af22393d4605f3849
Rafael Hayyim Medina Sosa <61524008+RHMS-bot@users.noreply.github.com>
2020-07-07 11:40:58 -0500

    Cola de prioridad circular con espacio común en C

---
+++ Colas/Cola de prioridad circular (espacio común)/Cola_prioridad_circular_ec.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Cola_prioridad_circular_ec.h"
+
+/*Cola de prioridad circular con espacio comun */
+/*Solo hay 5 prioridades, 1 es de mayor prioridad y 5 es de menor prioridad*/
+		
+	int main(void){
+			int priori=0;
+			int opc=0;
+			int creada=0;
+			int dato=0;
+			struct cola *Cola=NULL;
+			printf("----------Cola de prioridad circular con espacio comun----------\n");
+			
+			while(opc != 4){
+			printf("\nEscoja una de las siguientes opciones:\n");
+			printf("1.-Agregar\n2.-Mostrar\n3.-Eliminar\n4.-Salir\n");
+			scanf("%d",&opc);
+			if(opc==1){
+				if(creada==0){
+					printf("\nEscriba el dato que va a encolar:\n");
+					scanf("%d",&dato);
+					printf("\nEscriba la prioridad del dato que va a encolar:\n");
+					scanf("%d",&priori);
+					if(priori<0 || priori>5)
+					printf("\nSolo puede escribir una prioridad desde 1 hasta 5");
+					else{
+					Cola=crear(Cola,dato,priori);
+					if(Cola ==NULL){
+			        	printf("\nNo se pudo crear la cola\n");
+				        return 0;
+			        }
+					creada=1;
+				   }
+				}
+				else{
+					printf("\nEscriba el dato que va a encolar:\n");
+					scanf("%d",&dato);
+					printf("\nEscriba la prioridad del dato que va a encolar:\n");
+					scanf("%d",&priori);
+					if(priori<0 || priori>5)
+					printf("\nSolo puede escribir una prioridad desde 1 hasta 5");
+					else
+					encolar(Cola,dato,priori);
+				}
+			}
+			if(opc==2){
+				if(Cola==NULL)
+					printf("\nNo hay una cola\n");
+				else
+				mostrar(Cola);
+			}
+			if(opc==3){
+				if(Cola==NULL)
+					printf("\nNo hay una cola\n");
+				else
+					desencolar(Cola);
+			}	
+			if(opc<1 || opc>4)
+				printf("Opcion no valida");
+			}
+					
+			return 0;
+		}
+		
