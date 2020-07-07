8a66549debecdd9b96c110502faa8438270b8266
Rafael Hayyim Medina Sosa <61524008+RHMS-bot@users.noreply.github.com>
2020-07-07 11:31:24 -0500

    Tabla Hash abierta en C

---
+++ Tabla Hash/Tabla Hash (abierta)/Tabla_Hash.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+	#include <stdlib.h>
+	#include "Tabla_Hash.h"
+	/*Medina Sosa Rafael Hayyim*/
+	
+	
+	int main(void){
+		struct TablaHash * tabla= NULL;
+		tabla=creartabla();
+		int opc=0;
+		int dato=0;
+		printf("----------Tabla Hash----------\n");
+		do{
+			printf("\nTeclea el numero de la operacion a realizar");
+			printf("\n1.-Agregar\n2.-Mostrar tabla\n3.-Eliminar\n4.-Buscar\n5.-Salir\n");
+			scanf("%d",&opc);
+			
+			switch(opc){
+			case 1:
+			printf("\nTeclea el dato entero que vas a colocar en la tabla\n");
+			scanf("%d",&dato);
+			insertar(tabla,dato);
+			break;
+			case 2:
+			mostrar(tabla);
+			break;
+			case 3:
+			 printf("\nTeclea el dato entero que vas a eliminar de la tabla\n");
+			 scanf("%d",&dato);
+			 eliminar(tabla,dato);
+			break;
+			case 4:
+			 printf("\nTeclea el dato entero que vas a buscar en la tabla\n");
+			 scanf("%d",&dato);
+			 buscar(tabla,dato);
+			break;
+			case 5:
+			printf("\nEjecucion terminada");
+			break;
+			default:
+				printf("Teclea un numero valido");
+		   }	
+		}
+		while(opc != 5);
+		
+		
+		return 0;
+	}
