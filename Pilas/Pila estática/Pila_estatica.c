f232cd76ffa3e7d145e28fc6913a8a9792be8864
Rafael Hayyim Medina Sosa <61524008+RHMS-bot@users.noreply.github.com>
2020-07-19 21:55:39 -0500

    Indentado Pila_estatica.c
    Indentado Pila_estatica.c

--- Pilas/Pila estática/Pila_estatica.c
+++ Pilas/Pila estática/Pila_estatica.c
@@ -1,35 +1,35 @@
 #include "Pila_estatica.h"
 
-int push(int pila[],int dato,int tope){
-	if(tope==99){
-		printf("\n La pila esta llena, no puedes meter mas datos");
-	}else{
-	pila[++tope]=dato;
-	return tope;
+int push(int pila[], int dato, int tope){
+  if(tope == 99){
+    printf("\n La pila esta llena, no puedes meter mas datos");
+  } 
+  else{
+    pila[++tope]=dato;
+    return tope;
   }
 }
 
-void mostrar(int pila[],int tope){
-	int i=0;
-	if(tope == -1){
-	 printf("\nNo hay una pila que mostrar");
-		return;
-	}
-	while(i<=tope){
-		printf("%d,",pila[i]);
-		++i;
-	}
-	printf("\n");
+void mostrar(int pila[], int tope){
+  int i=0;
+  if(tope == -1){
+    printf("\nNo hay una pila que mostrar");
+    return;
+  }
+  while(i <= tope){
+    printf("%d,", pila[i]);
+    ++i;
+  }
+  printf("\n");
 }
 
-int pop(int pila[],int tope){
-	if(tope == -1){
-		printf("\nNo hay elementos en la pila");
-		return tope;
-	}
-	pila[tope]=0;
-	tope=tope-1;
-	
-	return tope;
-}
+int pop(int pila[], int tope){
+  if (tope == -1){
+    printf("\nNo hay elementos en la pila");
+    return tope;
+  }
+  pila[tope]=0;
+  tope=tope-1;
 
+  return tope;
+}
