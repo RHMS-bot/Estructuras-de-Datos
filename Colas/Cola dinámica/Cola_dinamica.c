7483dbbd35f5d0aac40157778346bc0e8b128d66
Rafael Hayyim Medina Sosa <61524008+RHMS-bot@users.noreply.github.com>
2020-07-21 20:28:30 -0500

    Indentado Cola_dinamica.c
    Indentado Cola_dinamica.c

--- Colas/Cola dinámica/Cola_dinamica.c
+++ Colas/Cola dinámica/Cola_dinamica.c
@@ -1,48 +1,48 @@
 #include "Cola_dinamica.h"
 
 struct Cola * crear(int dato){
-	struct Cola *nuevo=NULL;
-	nuevo=(struct Cola *)malloc(sizeof(struct Cola));
-	
-	if(nuevo==NULL){
-		printf("\nNo hay memoria sufuciente");
-		return NULL;
-	}
-	
-	nuevo->sig=NULL;
-	nuevo->dato=dato;
+    struct Cola * nuevo=NULL;
+    nuevo=(struct Cola *)malloc(sizeof(struct Cola));
+
+    if(nuevo == NULL){
+        printf("\nNo hay memoria sufuciente");
+        return NULL;
+    }
+
+    nuevo->sig=NULL;
+    nuevo->dato=dato;
 }
 
-struct Cola * encolar(struct Cola * cola,int dato){
+struct Cola * encolar(struct Cola * cola, int dato) {
     struct Cola * nuevo=NULL;
     struct Cola * aux=cola;
-    while(aux->sig != NULL){
-    	aux=aux->sig;
-	}
-	nuevo=crear(dato);
-	aux->sig=nuevo;
-	
-	return cola;
+    while (aux->sig != NULL) {
+        aux=aux->sig;
+    }
+    nuevo=crear(dato);
+    aux->sig=nuevo;
+
+    return cola;
 }
 
-void mostrar(struct Cola * cola){
-	if(cola==NULL){
-		printf("\nNo hay una cola que mostrar");
-		return;
-	}
-	printf("\n");
-	while(cola != NULL){
-		printf("%d,",cola->dato);
-		cola=cola->sig;
-	}
+void mostrar(struct Cola * cola) {
+    if (cola == NULL) {
+        printf("\nNo hay una cola que mostrar");
+        return;
+    }
+    printf("\n");
+    while (cola != NULL) {
+        printf("%d,", cola->dato);
+        cola=cola->sig;
+    }
 }
 
-struct Cola * desencolar(struct Cola * cola){
-	if(cola==NULL){
-		printf("\nNo hay una cola de la cual eliminar");
-		return NULL;
-	}
-	struct Cola * aux=cola->sig;
-	free(cola);
-	return aux;
+struct Cola * desencolar(struct Cola * cola) {
+    if(cola == NULL) {
+        printf("\nNo hay una cola de la cual eliminar");
+        return NULL;
+    }
+    struct Cola * aux=cola->sig;
+    free(cola);
+    return aux;
 }
